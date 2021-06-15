fd15b6852ee21f51d26b8dc834c01a52c961bd26
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-06-15 21:55:43 +0200

    feat(index.js): index configure and run bot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+const bot = require('./bot')
+
+require('dotenv').config()
+
+
+console.info('[✅]> Bot enebled...')
+
+if (process.env.TOKEN == undefined) {
+  console.error(`[⛔️]> Token is undefined!`)
+  return
+}
+
+if (process.env.PREFIX == undefined)
+  console.warn(`[⚡]> Prefix isn't defined in .env file...`)
+
+if (process.env.PREFIX != undefined) bot.prefix = process.env.PREFIX
+
+console.info(`[✅]> Default prefix is '${bot.prefix}'.`)
+
+bot.login(process.env.TOKEN)
+
+console.log('Connected to Discord...')
