dbc0f4fbb1288027a49c1ab32d50e6b8bfa62f85
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-06-16 12:12:49 +0200

    feat(index.js): emoji-logs implementation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-const bot = require('./bot')
-
+require('emoji-logs').config({})
 require('dotenv').config()
 
-console.info('[✅]> Bot enebled...')
+const bot = require('./bot')
+
+console.info('Bot enebled...')
 
 if (process.env.TOKEN == undefined) {
-  console.error(`[⛔️]> Token is undefined!`)
+  console.error(`Token is undefined!`)
   return
 }
 
 if (process.env.PREFIX == undefined)
-  console.warn(`[⚡]> Prefix isn't defined in .env file...`)
+  console.warn(`Prefix isn't defined in .env file...`)
 
 if (process.env.PREFIX != undefined) bot.prefix = process.env.PREFIX
 
-console.info(`[✅]> Default prefix is '${bot.prefix}'.`)
+console.info(`Default prefix is '${bot.prefix}'.`)
 
 bot.login(process.env.TOKEN)
 
-console.log('Connected to Discord...')
+console.info('Connected to Discord...')
