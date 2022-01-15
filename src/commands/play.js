5719b8f4ae8fb6d00f9751639ecb8750e1b2aa6f
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-01-15 14:38:48 +0100

    feat(commands/play): validate argument

--- src/commands/play.js
+++ src/commands/play.js
@@ -4,11 +4,17 @@ module.exports = {
   async execute({ message, args }) {
     if (!args[0])
       return message.channel.send('**You did not choose an action!**')
+
+    const actions = ['rock', 'paper', 'scissors']
+
+    if (!actions.includes(args[0]))
+      return message.channel.send(
+        '**What is that ?! :face_with_raised_eyebrow:**'
+      )
+
     const user = message.guild.members.cache.get(message.author.id).tag
     const userAction = args[0].toLowerCase()
-    const botAction = ['rock', 'paper', 'scissors'][
-      Math.floor(Math.random() * 3)
-    ]
+    const botAction = actions[Math.floor(Math.random() * 3)]
 
     let result
 
